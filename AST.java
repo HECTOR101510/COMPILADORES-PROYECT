c6970dd96d67452ea634be49db13b4ac3aae8f18
Hector Ruiz <ruizhernandezhector57@gmail.com>
2023-11-19 00:02:59 -0600

    actualizo AST con funciones nuevas

--- AST.java
+++ AST.java
@@ -135,8 +135,298 @@ public class AST implements Parser{
     private void ASSIGNMENT(){
         if(hayErrores)
             return;
+        switch (preanalisis.tipo) {
+            case BANG,MINUS,TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                LOGIC_OR();
+                ASSIGNMENT_OPC();
+                break;
+        
+            default:
+                break;
+        }
     }
 
+    private void LOGIC_OR(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case BANG,MINUS,TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                LOGIC_AND();
+                LOGIC_OR_2();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    private void LOGIC_OR_2(){
+        if(hayErrores)
+            return;
+        if(preanalisis.tipo==TipoToken.OR){
+            match(TipoToken.OR);
+            LOGIC_AND();
+            LOGIC_OR_2();
+        }
+    }
+
+    private void LOGIC_AND(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case BANG,MINUS,TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                EQUALITY();
+                LOGIC_AND_2();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    private void LOGIC_AND_2(){
+        if(hayErrores)
+            return;
+        if(preanalisis.tipo==TipoToken.AND){
+            match(TipoToken.AND);
+            EQUALITY();
+            LOGIC_AND_2();
+        }
+    }
+
+    private void EQUALITY(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case BANG,MINUS,TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                COMPARISON();
+                EQUALITY_2();
+                break;
+        
+            default:
+                break;
+        }
+    }
+    private void EQUALITY_2(){
+        if(preanalisis.tipo==TipoToken.BANG_EQUAL){
+            match(TipoToken.BANG_EQUAL);
+            COMPARISON();
+            EQUALITY_2();
+        }
+        else if(preanalisis.tipo==TipoToken.EQUAL_EQUAL){
+            match(TipoToken.EQUAL_EQUAL);
+            COMPARISON();
+            EQUALITY_2();
+        }
+    }
+    private void COMPARISON(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case BANG,MINUS,TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                TERM();
+                COMPARISON_2();
+                break;
+        
+            default:
+                break;
+        }
+    }
+        private void COMPARISON_2(){
+            if(preanalisis.tipo==TipoToken.GREATER){
+                match(TipoToken.GREATER);
+                TERM();
+                COMPARISON_2();
+            }
+            else if(preanalisis.tipo==TipoToken.GREATER_EQUAL){
+                match(TipoToken.GREATER_EQUAL);
+                TERM();
+                COMPARISON_2();
+            }
+            else if(preanalisis.tipo==TipoToken.LESS){
+                match(TipoToken.LESS);
+                TERM();
+                COMPARISON_2();
+            }
+            else if(preanalisis.tipo==TipoToken.LESS_EQUAL){
+                match(TipoToken.LESS_EQUAL);
+                TERM();
+                COMPARISON_2();
+            }
+        }
+        private void TERM(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case BANG,MINUS,TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                FACTOR();
+                TERM_2();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    private void TERM_2(){
+        if(hayErrores)
+            return;
+        if(preanalisis.tipo==TipoToken.MINUS){
+            match(TipoToken.MINUS);
+            FACTOR();
+            TERM_2();
+        }
+        else if(preanalisis.tipo==TipoToken.PLUS){
+            match(TipoToken.PLUS);
+            FACTOR();
+            TERM_2();
+        }
+        else{
+            hayErrores=true;
+            System.out.println("Error:Se esperaba un + o  -");
+        }
+    }
+
+    private void FACTOR(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case BANG,MINUS,TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                UNARY();
+                FACTOR_2();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    private void FACTOR_2(){
+        if(preanalisis.tipo==TipoToken.SLASH){
+            match(TipoToken.SLASH);
+            UNARY();
+            FACTOR_2();
+        }
+        else if(preanalisis.tipo==TipoToken.STAR){
+            match(TipoToken.STAR);
+            UNARY();
+            FACTOR_2();
+        }
+        else{
+            hayErrores=true;
+            System.out.println("Error: Se esperaba un * o /");
+        }
+    }
+
+    private void UNARY(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case BANG:
+                match(TipoToken.BANG);
+                UNARY();
+                break;
+
+            case MINUS:
+                match(TipoToken.BANG);
+                UNARY();
+                break;
+            case TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                CALL();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    private void CALL(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case TRUE,FALSE,NULL,NUMBER,STRING,IDENTIFIER,LEFT_PAREN:
+                PRIMARY();
+                CALL_2();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void CALL_2(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case LEFT_PAREN:
+                match(TipoToken.LEFT_PAREN);
+                ARGUMENTS_OPC();
+                match(TipoToken.RIGHT_PAREN);
+                CALL_2();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ARGUMENTS_OPC(){
+        EXPRESSION();
+        ARGUMENTS();
+    }
+
+    private void ARGUMENTS(){
+        if(preanalisis.tipo==TipoToken.COMMA){
+            match(TipoToken.COMMA);
+            EXPRESSION();
+            ARGUMENTS();
+        }
+    }
+
+    private void PRIMARY(){
+        if(hayErrores)
+            return;
+        switch (preanalisis.tipo) {
+            case TRUE:
+                match(TipoToken.TRUE);
+                break;
+
+            case FALSE:
+                match(TipoToken.FALSE);
+                break;
+
+            case NULL:
+                match(TipoToken.NULL);
+                break;
+
+            case NUMBER:
+                match(TipoToken.NUMBER);
+                break;
+
+            case STRING:
+                match(TipoToken.STRING);
+                break;
+
+            case IDENTIFIER:
+                match(TipoToken.IDENTIFIER);
+                break;
+            
+            case LEFT_PAREN:
+                match(TipoToken.LEFT_PAREN);
+                EXPRESSION();
+                match(TipoToken.RIGHT_PAREN);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ASSIGNMENT_OPC(){
+        if(hayErrores)
+            return;
+        if(preanalisis.tipo==TipoToken.EQUAL){
+            match(TipoToken.EQUAL);
+            EXPRESSION();
+        }
+    }
 
     private void match(TipoToken tt) {
         if (i < tokens.size()) {
